6c3871e11d7d3066bfdf8a50b415fcdecd1ab2ad
anazanz <75966913+anazanz@users.noreply.github.com>
2020-12-17 14:38:53 +0530

    Add files via upload

---
+++ sort1.py
@@ -0,0 +1,11 @@
+list=[('anaz','id:2','9897564323'),
+      ('thahseen','id:8','9876545623'),
+      ('ameer','id:3','9876437654'),
+      ('bilal','id:5','8756349856'),
+      ('abu','id:9','9876453212')]
+
+list.sort(key=lambda list:list[0])
+for x in list:
+
+ print(x)
+
